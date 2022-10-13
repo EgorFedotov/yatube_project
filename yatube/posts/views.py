5b1a23a3b2318e9828436dc12839e977af96d84f
Егор Федотов <egorfedotovarz@gmail.com>
2022-10-13 15:33:40 +0300

    sprint3 topic 4 lesson4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Самая главная страница')
 
-def group_posts(request, any_slug):
-    return HttpResponse('Тут будут посты')
+def index(request):
+    templates = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, templates, context)
+
+
+def group_posts(request):
+    templates = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, templates, context)
