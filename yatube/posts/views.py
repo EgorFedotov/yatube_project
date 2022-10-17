f8e8fd2156719840e20760ae3bf855fb8c954bfd
Егор Федотов <egorfedotovarz@gmail.com>
2022-10-17 14:54:25 +0300

    the end

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
+COUNT_POSTS: int = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:COUNT_POSTS]
     context = {
+        'title': title,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
+    title = 'Записи сообщества'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:COUNT_POSTS]
     context = {
+        'title': title,
         'group': group,
         'posts': posts,
     }
